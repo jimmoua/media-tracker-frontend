d966d6f1922355c4fab61b2a7f9f7a3b3d938ec2
Jim Moua <jimmoua24@gmail.com>
2021-02-28 07:10:23 -0600

    Homepage.jsx added

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,13 +1,63 @@
 import React from "react";
+import Loader from "react-spinners/BeatLoader";
+import ShowError from "./ShowError";
+import { fetchMediaList } from "./api";
 
 function Homepage() {
+  const [list, setList] = React.useState();
+
+  React.useEffect(() => {
+    (async() => {
+      const mList = await fetchMediaList();
+      if(mList instanceof Object) {
+        setList(mList);
+      } else {
+        setList(null);
+      }
+    })();
+  });
+
+  /**
+   * @function renderList
+   * @description
+   * If HTTP returns a list object, return the list. Otherwise, return the element for an error.
+   * @returns {JSX.Element}
+   */
+  function renderList() {
+    if(list === null) {
+      return <ShowError />;
+    } else if(!list) {
+      return (
+        <>
+          <div>Fetching your list. Please wait.</div>
+          <div className="container">
+            <div className="column">
+              <Loader />
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          {JSON.stringify(list)}
+        </>
+      );
+    }
+  }
+
   return (
-    <section className="hero is-primary is-medium">
-      <div className="hero-body">
-        <p className="title">Media Tracker</p>
-        <p className="subtitle">Tracking your favorite shows and comics...</p>
+    <>
+      <div className="hero is-primary is-medium">
+        <div className="hero-body">
+          <p className="title">Media Tracker</p>
+          <p className="subtitle">because I never know where I&apos;m at {"¯\\_(ツ)_/¯"}</p>
+        </div>
       </div>
-    </section>
+      <section className="section is-mobile has-text-centered">
+        {renderList()}
+      </section>
+    </>
   );
 }
 
