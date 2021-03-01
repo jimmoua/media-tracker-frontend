fef6e4a7d00e0f84c4510761efacf2a5f2e8ca9e
Jim Moua <jimmoua24@gmail.com>
2021-03-01 00:48:24 -0600

    Media search implemented.

--- src/ListTable.jsx
+++ src/ListTable.jsx
@@ -8,18 +8,17 @@ import TableRow from "./TableRow";
  * @returns {JSX.Element}
  */
 const ListTable = ({ mediaList, modalHandler }) => {
-  const [tableList, setTableList] = React.useState(mediaList);
   const [searchField, setSearchField] = React.useState("");
-
-  // 0 = none, 1 = media name, 2 = media type
-  const [sortBy, setSortByField] = React.useState(0);
 
   function sortListMediaListByTitle(list) {
     return list.sort((a, b) => a.title > b.title ? 1 : ((b.title > a.title) ? -1 : 0));
   }
+  function filterListMediaByTitle(list, mediaTitle) {
+    return list.filter(e => e.title.includes(mediaTitle));
+  }
 
   function createTableRows() {
-    const _t = sortListMediaListByTitle(mediaList);
+    const _t = sortListMediaListByTitle(filterListMediaByTitle(mediaList, searchField));
     const _list = [];
     for(let i = 0; i < _t.length; i++) {
       _list.push(
@@ -38,6 +37,22 @@ const ListTable = ({ mediaList, modalHandler }) => {
 
   return (
     <>
+      <div className="column is-half is-center container mb-5">
+        <div className="container">
+          <div className="field">
+            <input
+              type="text"
+              className="input has-text-centered is-bold"
+              value={searchField}
+              onChange={(e) => {
+                setSearchField(e.target.value);
+                createTableRows();
+              }}
+              placeholder={"Search for media"}
+            />
+          </div>
+        </div>
+      </div>
       <table className="table is-hoverable is-fullwidth">
         <thead>
           <tr>
